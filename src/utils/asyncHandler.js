8ed30098ed24af502f0eb5f17336df060432da23
Bhanu Pratap Singh <info2bhanusingh@example.com>
2026-02-21 15:30:50 +0530

    routes setup

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,12 @@
 // wrapper function that is going to be used everywhere.
 const asyncHandler = (requestHandler) => {
-    (req,res,next) => {
+    return (req,res,next) => {
         Promise.resolve(requestHandler(req,res,next)).
         catch((err) => next(err));
     }
+
 }
+
 
 
 export {asyncHandler};
